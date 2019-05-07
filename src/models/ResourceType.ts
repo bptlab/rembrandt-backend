d31bdd66cbdfefaba03f322ffe6503c4ae9bee28
MaximilianV <maximilian-voelker@t-online.de>
2019-05-07 15:28:55 +0200

    Add method for setting parent type by string for resource types

--- src/models/ResourceType.ts
+++ src/models/ResourceType.ts
@@ -1,4 +1,4 @@
-import { prop, Typegoose, Ref, pre } from 'typegoose';
+import { prop, Typegoose, Ref, pre, instanceMethod } from 'typegoose';
 
 interface Attribute {
   name: string;
@@ -27,6 +27,19 @@ export class ResourceType extends Typegoose {
 
   @prop({ ref: ResourceType })
   public parentType?: Ref<ResourceType>;
+
+  @instanceMethod
+  public async setParentResourceTypeByName(resourceTypeName: string): Promise<void> {
+    const parentResourceType = await ResourceTypeModel.findOne({ name: resourceTypeName });
+
+    if (!parentResourceType) {
+      return new Promise((resolve, reject) => {
+        reject(new Error(`Failed to find parent resource type definition for '${resourceTypeName}'!`));
+      });
+    }
+
+    this.parentType = parentResourceType._id;
+  }
 }
 
 const ResourceTypeModel = new ResourceType().getModelForClass(ResourceType);
