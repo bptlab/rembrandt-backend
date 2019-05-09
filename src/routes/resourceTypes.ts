486eaceee4087011fe981eef3c950cbadace58df
MaximilianV <maximilian-voelker@t-online.de>
2019-05-09 11:13:11 +0200

    Add serializer for error messages

--- src/routes/resourceTypes.ts
+++ src/routes/resourceTypes.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import ResourceType, { resourceTypeSerializer } from '@/models/ResourceType';
 import winston from 'winston';
+import createJSONError from '@/utils/errorSerializer';
 
 const router: express.Router = express.Router();
 
@@ -9,8 +10,8 @@ router.get('/', async (req: express.Request, res: express.Response) => {
     const resourceTypes = await ResourceType.find({}).exec();
     res.send(resourceTypeSerializer.serialize(resourceTypes));
   } catch (error) {
-    winston.error(error);
-    res.status(500).send(error);
+    winston.error(error.message);
+    res.status(500).send(createJSONError('500', 'Error in ResourceType-Router', error.message));
   }
 });
 
