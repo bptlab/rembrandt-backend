dfbec0fdcc753a2ac0e2112368ef0397c8e9c76c
Christian Friedow <christian@friedow.com>
2019-05-10 19:32:24 +0200

    Enforced content type as specified by jsonapi.org

--- src/app.ts
+++ src/app.ts
@@ -8,16 +8,31 @@ import resourceTypeRouter from '@/routes/resourceTypes';
 import resourceInstanceRouter from '@/routes/resourceInstances';
 import RootTypeInitializer from '@/utils/RootTypeInitializer';
 import ResourceInstanceInitializer from '@/utils/ResourceInstanceInitializer';
+import createJSONError from '@/utils/errorSerializer';
 
+// tslint:disable-next-line: no-var-requires
 const swaggerConfig = require('@/swagger.json');
+const contentType: string = 'application/vnd.api+json';
 
-async function startApiServer() {
+function enforceContentType(req: express.Request, res: express.Response, next: express.NextFunction): void {
+  if (!req.headers || req.headers['content-type'] !== contentType) {
+    res.status(415).send(createJSONError(
+      '415',
+      'Unsupported Media Type',
+      `Only requests specifying '${contentType}' as content type are allowed.`,
+    ));
+  }
+  next();
+}
+
+async function startApiServer(): Promise<void> {
   const app: express.Application = express();
   const port: string = process.env.PORT || '3000';
 
   app.use(bodyParser.urlencoded({extended: true}));
-  app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+  app.use(bodyParser.json({ type:  contentType}));
   app.use(cors());
+  app.use('/', enforceContentType);
   app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerConfig));
 
   app.use('/hello-world', helloWorldRouter);
@@ -35,6 +50,7 @@ async function startApiServer() {
 }
 
 const db = mongoose.connection;
+// tslint:disable-next-line: no-console
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', startApiServer);
 
