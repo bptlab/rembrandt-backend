f246d518866ba82eea03106ed945a19a92b273c3
MaximilianV <maximilian-voelker@t-online.de>
2019-07-11 14:23:50 +0200

    Only create test recipe if we don't have one yet

--- src/utils/testRecipe.ts
+++ src/utils/testRecipe.ts
@@ -1,43 +1,64 @@
-import { ObjectID } from 'bson';
-import Ingredient, { InputIngredient, OutputIngredient } from '@/models/Ingredient';
 import OptimizationTransformerModel from '@/models/OptimizationTransformer';
 import RecipeController from '@/controllers/RecipeController';
 import OptimizationAlgorithmModel from '@/models/OptimizationAlgorithm';
+import OptimizationRecipeModel, { OptimizationRecipe } from '@/models/OptimizationRecipe';
+import winston = require('winston');
 
-const IdOfResourceType = '5d14c2251b0b6d2cd4b4c52e';
+const idOfResourceType = '5d14c2251b0b6d2cd4b4c52e';
+const nameOfTestRecipe = 'Testrecipe';
 
 export async function testRecipe() {
-  const inputDefinition: InputIngredient = {
-    inputResourceType: new ObjectID(IdOfResourceType),
-  };
+  let testRecipeInstance = await OptimizationRecipeModel.findOne({ name: nameOfTestRecipe }).exec();
 
-  const transformerDefinition = await OptimizationTransformerModel.findOne({ resourceType: IdOfResourceType }).exec();
-  if (!transformerDefinition) {
-    throw new Error(`Could not find a transformer definition for resource type ${IdOfResourceType}`);
+  if (testRecipeInstance) {
+    winston.debug('You already have a test recipe in your database.');
+  } else {
+    winston.debug('Creating a new test recipe in your database.');
+
+    const transformerDefinition = await OptimizationTransformerModel.findOne({ resourceType: idOfResourceType }).exec();
+    if (!transformerDefinition) {
+      throw new Error(`Could not find a transformer definition for resource type ${idOfResourceType}`);
+    }
+
+    const algorithmDefinition = await OptimizationAlgorithmModel.findOne({ outputs: idOfResourceType }).exec();
+    if (!algorithmDefinition) {
+      throw new Error(`Could not find a algorithm definition for resource type ${idOfResourceType}`);
+    }
+
+    testRecipeInstance = new OptimizationRecipeModel();
+
+    const recipeJson = {
+      ingredientObject: {
+        definitionId: idOfResourceType,
+        type: 'output',
+      },
+      inputs: [{
+        ingredientObject: {
+          definitionId: algorithmDefinition.id,
+          type: 'algorithm',
+        },
+        inputs: [{
+          ingredientObject: {
+            definitionId: transformerDefinition.id,
+            type: 'transform',
+          },
+          inputs: [{
+            ingredientObject: {
+              definitionId: idOfResourceType,
+              type: 'input',
+            },
+          }],
+        }],
+      }],
+    };
+
+    OptimizationRecipe.addIngredientFromJSON(testRecipeInstance, recipeJson);
+    testRecipeInstance.name = nameOfTestRecipe;
+    await testRecipeInstance.save();
+    winston.debug(`New test recipe '${nameOfTestRecipe}' created and saved.`);
   }
-
-  const algorithmDefinition = await OptimizationAlgorithmModel.findOne({ outputs: IdOfResourceType }).exec();
-  if (!algorithmDefinition) {
-    throw new Error(`Could not find a algorithm definition for resource type ${IdOfResourceType}`);
-  }
-
-  const outputDefinition: OutputIngredient = {
-    outputResourceType: new ObjectID(IdOfResourceType),
-  };
-
-  const inputIngr = new Ingredient(inputDefinition);
-  const transformerIngr = new Ingredient(transformerDefinition);
-  const algorithmIngr = new Ingredient(algorithmDefinition);
-  const outputIngr = new Ingredient(outputDefinition);
-
-  inputIngr.outputs.push(transformerIngr);
-  (transformerIngr.inputs as Ingredient[]).push(inputIngr);
-  transformerIngr.outputs.push(algorithmIngr);
-  (algorithmIngr.inputs as Ingredient[]).push(transformerIngr);
-  algorithmIngr.outputs.push(outputIngr);
-  (outputIngr.inputs as Ingredient[]).push(algorithmIngr);
-
-  const recipe = new RecipeController([inputIngr, transformerIngr, algorithmIngr, outputIngr]);
-
-  await recipe.execute();
+  const recipeController = await RecipeController.createFromOptimizationIngredient(testRecipeInstance);
+  winston.debug(`Start executing test recipe '${nameOfTestRecipe}'...`);
+  await recipeController.execute();
+  winston.debug(`Finished test recipe '${nameOfTestRecipe}'...`);
 }
