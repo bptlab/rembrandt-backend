43bb2676d5ea09922e52555b31d64a7e209306d1
Sven <sven.ladusch@web.de>
2020-10-13 20:01:12 +0200

    fixed some warnings

--- src/controllers/FileController.ts
+++ src/controllers/FileController.ts
@@ -1,4 +1,4 @@
-//import { promises as fs } from 'fs';
+// import { promises as fs } from 'fs';
 import path, { resolve } from 'path';
 import config from '@/config.json';
 import IntermediateResult from '@/models/IntermediateResult';
