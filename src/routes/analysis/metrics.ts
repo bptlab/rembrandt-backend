b5f8325cfd84700e2de3056fc69bb9cc259525c0
Sven <sven.ladusch@web.de>
2020-10-25 19:12:34 +0100

    uses different api serialization library to account for dynamic keys

--- src/routes/analysis/metrics.ts
+++ src/routes/analysis/metrics.ts
@@ -1,11 +1,16 @@
 import winston from 'winston';
 import { Deserializer } from 'jsonapi-serializer';
-import { Serializer } from 'jsonapi-serializer';
-import apiSerializer from '@/utils/apiSerializer';
-import {metricResultSerializer} from '@/models/MetricResult';
 import createJSONError from '@/utils/errorSerializer';
 import express from 'express';
 import allocationLogger from '@/utils/allocationLogger';
+
+var JSONAPISerializer = require("json-api-serializer");
+var NewSerializer = new JSONAPISerializer({
+  convertCase: "camelCase",
+});
+NewSerializer.register("metricResult", {
+    id: "id",
+});
 
 const router: express.Router = express.Router();
 
@@ -15,11 +20,11 @@ router.post('/', async (req: express.Request, res: express.Response) => {
     console.log(queryJSON);
     if (queryJSON.query) {
       console.log(queryJSON.query);
-      const result = await allocationLogger.queryAllocationLog(queryJSON.query);
+      const result = await allocationLogger.queryDatabase(queryJSON.query);
       if (!result) {
         throw Error(`SQL query could not be processed: ${queryJSON.query} `);
       }
-      res.send(apiSerializer(result, metricResultSerializer));
+      res.send(NewSerializer.serialize('metricResult', result));
     }
   } catch (error) {
     winston.error(error.message);
